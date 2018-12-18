82963cfe7aeef0e5607f2e6eaccde05d779fb637
Joe Doperalski <jcdoperalski@gmail.com>
2018-12-18 16:21:37 -0700

    start of work to pull in reviews data

---
+++ v0.4/src/main_v04.py
@@ -0,0 +1,33 @@
+from hiking_data_v04 import DataGrabber, DataShaper, DatabaseExport
+from trail_recommender_v04 import Trail_Recommender
+import psycopg2
+import pandas.io.sql as sqlio
+from selenium.webdriver.firefox.options import Options
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+import psycopg2
+from sqlalchemy import create_engine
+from collections import defaultdict
+import re
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+
+exporter = DatabaseExport('az_trail_recommender')
+
+grabber = DataGrabber()
+grabber.grab_name_and_links()
+links = grabber.links_table
+
+exporter.database_pandas(grabber.links_table, 'links')
+
+trail_dict = grabber.grab_details(links)
---
+++ v0.4/src/trail_recommender_v04.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class Trail_Recommender():
+    
+    def __init__(self, dataframe):
+        
+        self.dataframe = dataframe 
+        self.cosine_mat = cosine_similarity(self.dataframe.drop(['trail_name', 'trail_id'], axis = 1), self.dataframe.drop(['trail_name', 'trail_id'], axis = 1))
+        
+    def recommend(self, trail):
+        indices = pd.Series(self.dataframe.index, index = self.dataframe['trail_name']).drop_duplicates()
+        idx = indices[trail]
+        sim_scores = list(enumerate(self.cosine_mat[idx]))
+        sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
+        sim_scores = sim_scores[1:11]
+        trail_indices = [i[0] for i in sim_scores]
+        return self.dataframe['trail_id'].iloc[trail_indices]
+
+if __name__ == "__main__":
+    pass
+    
